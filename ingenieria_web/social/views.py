1245ee1a9e2c7f4458d8f7f20b78182810cc9980
martin <mcasazza@kaitzen-solutions.com>
2018-10-04 11:47:43 -0300

    Icono nuevo en el Indice

--- ingenieria_web/social/views.py
+++ ingenieria_web/social/views.py
@@ -47,6 +47,12 @@ def register(request):
         if request.method == 'POST':
                 username = request.POST.get('username')
                 password = request.POST.get('password')
+                password2 = request.POST.get('password2')
+
+                if password != password2:
+                        messages.set_level(request, messages.WARNING)
+                        messages.add_message(request, CRITICAL, u'Las contrase\xf1as no son iguales')
+                        return render(request, 'adminlte/login.html' , {'error': messages} )
 
                 user = User.objects.create_user(username= request.POST.get('username'),email=request.POST.get('email'),password=request.POST.get('password'))
 
