2aef9298339a642ea4a1b198183d9e448f09ca94
Malta <niicou96@hotmail.com>
2018-10-08 15:38:30 -0300

    Scripts

--- ingenieria_web/urls.py
+++ ingenieria_web/urls.py
@@ -71,7 +71,7 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(template_name ='adminlte/registration/password_reset_confirm.html'), 
             name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name ='adminlte/registration/password_reset_complete.html'), name='password_reset_complete'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name ='adminlte/login.html'), name='password_reset_complete'),
 
 ]
 
