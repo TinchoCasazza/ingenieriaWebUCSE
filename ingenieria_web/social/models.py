7cef4d1cc8b0c4d01cdeec479169214eb86c95bb
Malta <niicou96@hotmail.com>
2018-10-11 13:09:21 -0300

    avatar/S.png

--- ingenieria_web/social/models.py
+++ ingenieria_web/social/models.py
@@ -24,7 +24,7 @@ class Skin(models.Model):
 class UserManager(AbstractUser):
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE, null=True)
     skinUser = models.ForeignKey(Skin, on_delete=models.CASCADE, null=True)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, default= '/avatars/s.png')
+    avatar = models.ImageField(upload_to="avatars/", blank=True, default= 'avatars/s.png')
 
 
 
