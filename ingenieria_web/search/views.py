f51e88fb536a7a556340587e9739f9831a78359c
Malta <niicou96@hotmail.com>
2018-10-26 16:16:50 -0300

    Fix menor busqueda

--- ingenieria_web/search/views.py
+++ ingenieria_web/search/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from ingenieria_web.search.documents import GrupoDocument
+from ingenieria_web.social.forms import NuevoGrupo
+from ingenieria_web.social.models import Suscripcion
 # Create your views here.
 
 def search(request):
     q = request.GET.get('q')
-
+    formNuevoGrupo = NuevoGrupo()
+    listaSuscripciones = Suscripcion.objects.all().order_by('-fecha_peticion')
     if q:
         grupos = GrupoDocument.search().query("match", NombreGrupo=q)
     else:
         grupos = ''
 
-    return render(request, 'adminlte/search.html', {'grupos':grupos})
+    return render(request, 'adminlte/search.html', {'grupos':grupos, 'formNuevoGrupo': formNuevoGrupo,'listaSuscripciones':listaSuscripciones})
